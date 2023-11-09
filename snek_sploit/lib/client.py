4d9d7a84de98e905fdde11955cbf9387d61192fd
Jiří Rája <jiri.raja@gmail.com>
2023-11-09 16:28:48 +0100

    removed rpc directory; reorganized the context variables; added option to pass token to context

--- snek_sploit/lib/client.py
+++ snek_sploit/lib/client.py
@@ -6,12 +6,12 @@ from snek_sploit.lib.auth import Auth
 
 class Client:
     def __init__(self, username: str, password: str, host: str = "127.0.0.1", port: int = 55553, uri: str = "/api/",
-                 ssl: bool = True, certificate: str = "", log_in: bool = True, disable_https_warnings: bool = False):
+                 ssl: bool = True, certificate: str = "", log_in: bool = True, token: str = "",
+                 disable_https_warnings: bool = False):
         if disable_https_warnings:
             urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-        self.context = Context(username, password, host, port, uri, ssl, certificate)
-
+        self.context = Context(username, password, host, port, uri, ssl, certificate, token)
         self.auth = Auth(self.context)
 
         if log_in:
--- snek_sploit/lib/context.py
+++ snek_sploit/lib/context.py
@@ -9,16 +9,15 @@ class Context:
                  ssl: bool = True, certificate: str = "", token: str = ""):
         self.username = username
         self.password = password
+        self.token = token
 
-        self.url = f"http{'s' if ssl else ''}://{host}:{port}{uri}"
-        self.headers = {"Content-type": "binary/message-pack"}
-        # MSF self-signed certificate;
+        self._url = f"http{'s' if ssl else ''}://{host}:{port}{uri}"
+        self._headers = {"Content-type": "binary/message-pack"}
+        # MSF self-signed certificate
         # TODO: https://github.com/rapid7/metasploit-framework/issues/15569#issuecomment-901158008
         #  From the msfrpc -h ...
         #  -c   (JSON-RPC) Path to certificate (default: /root/.msf4/msf-ws-cert.pem)
-        self.certificate = certificate if certificate != "" else False
-
-        self.token = token
+        self._certificate = certificate if certificate != "" else False
 
     def call(self, endpoint: str, arguments: list = None) -> dict:
         if arguments is None:
@@ -28,7 +27,7 @@ class Context:
             arguments = [self.token, *arguments]
 
         data = msgpack.dumps([endpoint, *arguments])
-        request = requests.post(self.url, data, headers=self.headers, verify=self.certificate)
+        request = requests.post(self._url, data, headers=self._headers, verify=self._certificate)
         response = msgpack.loads(request.content)
         print(response)  # TODO: remove, only for quick and dirty debugging
 
