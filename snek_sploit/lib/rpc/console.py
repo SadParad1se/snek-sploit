4916fb56f27992ec64da3424fc0a8cf8b7319a6e
Jiri Raja <jiri.raja@gmail.com>
2023-11-13 18:17:45 +0100

    separated console data and info parsing; updated docstrings

--- snek_sploit/lib/rpc/console.py
+++ snek_sploit/lib/rpc/console.py
@@ -9,6 +9,12 @@ class ConsoleInfo:
     id: str
     prompt: bytes
     busy: bool
+
+
+@dataclass
+class ConsoleData:
+    prompt: bytes
+    busy: bool
     data: bytes
 
 
@@ -26,24 +32,36 @@ class RPCConsole(Base):
     SESSION_DETACH = "console.session_detach"
 
     @staticmethod
-    def _parse_console_data(response: dict) -> ConsoleInfo:
+    def _parse_console_info(response: dict) -> ConsoleInfo:
         """
-
-        :param response:
-        :return:
+        Get console information from the response.
+        :param response: Response containing information about the console
+        :return: Information about the console
         """
         return ConsoleInfo(
-            response.get(constants.ID),
+            response[constants.ID],
+            response[constants.PROMPT],
+            response[constants.BUSY]
+        )
+
+    @staticmethod
+    def _parse_console_data(response: dict) -> ConsoleData:
+        """
+        Get console data from the response.
+        :param response: Response containing information and data about the console
+        :return: Information about the console and its data
+        """
+        return ConsoleData(
             response[constants.PROMPT],
             response[constants.BUSY],
-            response.get(constants.DATA)
+            response[constants.DATA]
         )
 
     def create(self, options: dict = None) -> ConsoleInfo:
         """
-
-        :param options:
-        :return:
+        Create a new framework console instance.
+        :param options: Options used for creating the console
+        :return: Information about the console
         :full response example: {b'id': '3', b'prompt': b'', b'busy': False}
         """
         # TODO: what options are supported?
@@ -53,27 +71,27 @@ class RPCConsole(Base):
 
         response = self._context.call(self.CREATE, [options])
 
-        return self._parse_console_data(response)
+        return self._parse_console_info(response)
 
     def list_consoles(self) -> list[ConsoleInfo]:
         """
-
-        :return:
+        List framework consoles.
+        :return: List of framework consoles
         :full response example: {b'consoles': [{b'id': '1', b'prompt': b'msf6 > ', b'busy': False}]}
         """
         response = self._context.call(self.LIST)
 
         consoles = []
         for console in response[constants.CONSOLES]:
-            consoles.append(self._parse_console_data(console))
+            consoles.append(self._parse_console_info(console))
 
         return consoles
 
     def destroy(self, console_id: int) -> bool:
         """
-
-        :param console_id:
-        :return:
+        Delete a framework console instance.
+        :param console_id: ID of the console
+        :return: True in case of success
         :full response example: {b'result': b'success'}
         """
         response = self._context.call(self.DESTROY, [console_id])
@@ -83,11 +101,11 @@ class RPCConsole(Base):
 
         return response[constants.RESULT] == constants.SUCCESS
 
-    def read(self, console_id: int) -> ConsoleInfo:
+    def read(self, console_id: int) -> ConsoleData:
         """
-
-        :param console_id:
-        :return:
+        Read output from the console.
+        :param console_id: ID of the console
+        :return: Output from the console
         :full response example: {b'data': b"... https://docs.metasploit.com/\n\n", b'prompt': b'msf6 > ', b'busy': True}
         """
         response = self._context.call(self.READ, [console_id])
@@ -99,10 +117,10 @@ class RPCConsole(Base):
 
     def write(self, console_id: int, data: str) -> int:
         """
-
-        :param console_id:
-        :param data:
-        :return:
+        Send an input (such as a command) to the framework console.
+        :param console_id: ID of the console
+        :param data: Input to be written
+        :return: Number of bytes sent
         :full response example: {b'wrote': 11}
         """
         data += "\r\n"
@@ -116,10 +134,10 @@ class RPCConsole(Base):
 
     def tabs(self, console_id: int, line: str) -> list[bytes]:
         """
-
-        :param console_id:
-        :param line:
-        :return:
+        Get tab-completed version of your input (such as a module path).
+        :param console_id: ID of the console
+        :param line: Line you want to complete
+        :return: Possible tab-completions for your input
         :full response example: {b'tabs': [b'use exploit/windows/smb/ms08_067_netapi']}
         """
         response = self._context.call(self.TABS, [console_id, line])
@@ -131,9 +149,10 @@ class RPCConsole(Base):
 
     def session_kill(self, console_id: int) -> bool:
         """
-
-        :param console_id:
-        :return:
+        Kill a framework session. This serves the same purpose as CTRL+C to abort an interactive session.
+        Consider using the session API calls instead.
+        :param console_id: ID of the console
+        :return: True in case of success
         :full response example: {b'result': b'success'}
         """
         response = self._context.call(self.SESSION_KILL, [console_id])
@@ -145,9 +164,9 @@ class RPCConsole(Base):
 
     def session_detach(self, console_id: int) -> bool:
         """
-
-        :param console_id:
-        :return:
+        Detache a framework session. This serves the same purpose as CTRL+Z to background an interactive session.
+        :param console_id: ID of the console
+        :return: True in case of success
         :full response example: {b'result': b'success'}
         """
         response = self._context.call(self.SESSION_DETACH, [console_id])
