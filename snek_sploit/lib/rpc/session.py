b954db07f4e145ff73087b68abb54b2a68274a34
Jiri Raja <jiri.raja@gmail.com>
2023-11-21 15:01:04 +0100

    partial session RPC update

--- snek_sploit/lib/rpc/session.py
+++ snek_sploit/lib/rpc/session.py
@@ -1,10 +1,49 @@
 from typing import List, Dict
+from dataclasses import dataclass
 
 from snek_sploit.lib.base import Base
 from snek_sploit.util import constants
 
 
-# TODO unfinished, untested
+@dataclass
+class SessionInformation:
+    """
+    Information about a session.
+
+    Parameters:
+        type: Payload type. Example: meterpreter
+        tunnel_local: Tunnel (where the malicious traffic comes from)
+        tunnel_peer: Tunnel (local)
+        via_exploit: Name of the exploit used by the session
+        desc: Session description
+        info: Session info (most likely the target's computer name)
+        workspace: Name of the workspace
+        session_host: Session host
+        session_port: Session port
+        target_host: Target host
+        username: Username
+        uuid: UUID
+        exploit_uuid: Exploit's UUID
+        routes: Routes
+        platform: Platform
+    """
+    type: str
+    tunnel_local: str
+    tunnel_peer: str
+    via_exploit: str
+    desc: str
+    info: str
+    workspace: str
+    session_host: str
+    session_port: int
+    target_host: str
+    username: str
+    uuid: str
+    exploit_uuid: str
+    routes: str
+    platform: str
+
+
 class RPCSession(Base):
     """
     https://docs.metasploit.com/api/Msf/RPC/RPC_Session.html
@@ -29,49 +68,68 @@ class RPCSession(Base):
     RING_LAST = "session.ring_last"
     RING_CLEAR = "session.ring_clear"
 
-    def list_sessions(self, *args) -> object:
-        """
+    @staticmethod
+    def _parse_session_information(response: Dict[str, str]) -> SessionInformation:
+        return SessionInformation(
+            response[constants.B_TYPE]
+        )
 
+    def list_sessions(self) -> Dict[int, SessionInformation]:
+        """
+        Get a list of sessions that belong to the framework instance used by the RPC service.
+        :return: Available sessions
+        :full response example:
+        """
+        response = self._context.call(self.LIST)
+
+        return {key: self._parse_session_information(value) for key, value in response.items()}
+
+    def stop(self, session_id: int) -> bool:
+        """
+        Stop a session - alias for killing a session in msfconsole.
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
-        response = self._context.call(self.LIST, list(args))
+        response = self._context.call(self.STOP, [session_id])
 
-        return response
+        return response[constants.B_RESULT] == constants.B_SUCCESS
 
-    def stop(self, *args) -> object:
+    def shell_read(self, session_id: int, pointer: int = None) -> object:
         """
-
+        Read the output of a shell session (such as a command output).
+        :param session_id: ID of the session
+        :param pointer: Read from/Offset?  # TODO
         :return:
         :full response example:
         """
-        response = self._context.call(self.STOP, list(args))
+        response = self._context.call(self.SHELL_READ, [session_id, pointer])
 
         return response
 
-    def shell_read(self, *args) -> object:
+    def shell_write(self, session_id: int, data: str, add_new_line: bool = True) -> object:
         """
-
+        Write to a shell session (such as a command).
+        Note that you will to manually add a newline at the enf of your input so the system will process it.
+        :param session_id: ID of the session
+        :param data: Data (command) to write
+        :param add_new_line: Whether to add a new line at the end of the data
         :return:
         :full response example:
         """
-        response = self._context.call(self.SHELL_READ, list(args))
+        if add_new_line:
+            data += "\r\n"
+
+        response = self._context.call(self.SHELL_WRITE, [session_id, data])
 
         return response
 
-    def shell_write(self, *args) -> object:
+    def shell_upgrade(self, session_id: int, local_host: str, local_port: int) -> object:
         """
 
-        :return:
-        :full response example:
-        """
-        response = self._context.call(self.SHELL_WRITE, list(args))
-
-        return response
-
-    def shell_upgrade(self, *args) -> object:
-        """
-
+        :param session_id: ID of the session
+        :param local_host: Local host
+        :param local_port: Local port
         :return:
         :full response example:
         """
@@ -79,9 +137,10 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_read(self, *args) -> object:
+    def meterpreter_read(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -89,9 +148,10 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_write(self, *args) -> object:
+    def meterpreter_write(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -99,9 +159,10 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_session_detach(self, *args) -> object:
+    def meterpreter_session_detach(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -109,9 +170,10 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_session_kill(self, *args) -> object:
+    def meterpreter_session_kill(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -119,9 +181,10 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_tabs(self, *args) -> object:
+    def meterpreter_tabs(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -129,9 +192,10 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_run_single(self, *args) -> object:
+    def meterpreter_run_single(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -139,19 +203,18 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_script(self, *args) -> object:
+    def meterpreter_script(self, session_id: int, data: ) -> object:
         """
-
-        :return:
-        :full response example:
+        !!!DEPRECATED!!!  # TODO: remove?
         """
         response = self._context.call(self.METERPRETER_SCRIPT, list(args))
 
         return response
 
-    def meterpreter_transport_change(self, *args) -> object:
+    def meterpreter_transport_change(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -159,9 +222,10 @@ class RPCSession(Base):
 
         return response
 
-    def meterpreter_directory_separator(self, *args) -> object:
+    def meterpreter_directory_separator(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -169,9 +233,10 @@ class RPCSession(Base):
 
         return response
 
-    def compatible_modules(self, *args) -> object:
+    def compatible_modules(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -179,9 +244,10 @@ class RPCSession(Base):
 
         return response
 
-    def ring_read(self, *args) -> object:
+    def ring_read(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -189,9 +255,10 @@ class RPCSession(Base):
 
         return response
 
-    def ring_put(self, *args) -> object:
+    def ring_put(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -199,9 +266,10 @@ class RPCSession(Base):
 
         return response
 
-    def ring_last(self, *args) -> object:
+    def ring_last(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
@@ -209,9 +277,10 @@ class RPCSession(Base):
 
         return response
 
-    def ring_clear(self, *args) -> object:
+    def ring_clear(self, session_id: int) -> object:
         """
 
+        :param session_id: ID of the session
         :return:
         :full response example:
         """
