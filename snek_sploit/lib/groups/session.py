a060ea0db9ca4e9ab5001bb05eb7f2183dd6ad84
Jiri Raja <jiri.raja@gmail.com>
2023-11-24 09:05:18 +0100

    Added a base class for a session instance and session classes

--- snek_sploit/lib/groups/session.py
+++ snek_sploit/lib/groups/session.py
@@ -1,6 +1,43 @@
-from snek_sploit.lib.base import Base, Context
-from snek_sploit.lib.rpc import RPCSession
+from abc import ABC, abstractmethod
 
+from snek_sploit.lib.base import Base, Context
+from snek_sploit.lib.rpc import RPCSession, SessionInformation
+
+
+class BaseSession(ABC):
+    def __init__(self, rpc: RPCSession, session_id: int):
+        self.rpc = rpc
+        self.id = session_id
+        try:
+            self.info = self.fetch_information()
+        except Exception:
+            raise Exception(f"Unable to fetch information about the session with id {self.id}. Make sure it exists.")
+
+    def fetch_information(self) -> SessionInformation:
+        return self.rpc.list_sessions()[self.id]
+
+    @abstractmethod
+    def write(self, data: str) -> bool:
+        pass
+
+    @abstractmethod
+    def read(self) -> str:
+        pass
+
+
+class ShellSession(BaseSession):
+    def write(self, data: str) -> bool:
+        pass
+
+    def read(self) -> str:
+        pass
+
+
+class MeterpreterSession(BaseSession):
+    pass
+
+
+class RingSession(BaseSession):
 
 class Session(Base):
     def __init__(self, context: Context):
