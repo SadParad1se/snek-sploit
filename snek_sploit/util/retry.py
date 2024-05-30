59e5b644048258e507986766f4eb96b623b009c5
Jiri Raja <jiri.raja@gmail.com>
2024-05-30 16:11:55 +0200

    renamed tries to attempts; execute the method correct number of times; on error try to wait

--- snek_sploit/util/retry.py
+++ snek_sploit/util/retry.py
@@ -1,27 +1,29 @@
 from functools import wraps, partial
+import time
 
 
-def retry(func=None, *, tries: int = 1, on_errors: tuple = None):
+def retry(func=None, *, attempts: int = 1, on_errors: tuple = None):
     """
     Retry a function if an error occurs.
     :param func: Original function
-    :param tries: Number of times to retry
+    :param attempts: Number of times to retry
     :param on_errors: On what errors to retry
     :return: Wrapper
     """
     if func is None:
-        return partial(retry, tries=tries, on_errors=on_errors)
+        return partial(retry, attempts=attempts, on_errors=on_errors)
 
     if not on_errors:
         on_errors = (Exception,)
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        for i in range(tries + 1):
+        for i in range(attempts):
             try:
                 return func(*args, **kwargs)
             except on_errors as ex:
-                if i == tries:
+                if i + 1 == attempts:
                     raise ex
+                time.sleep(min(i + 1 * 2, 30))
 
     return wrapper
